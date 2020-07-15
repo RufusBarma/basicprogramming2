b2d1b4817e2dc303ba5588771fb601cecc3d10a8
RufusBarma <neymanyu@gmail.com>
2020-07-15 15:02:48 +0500

    Add delegate syntax 2 task

---
+++ delegate syntax 2/delegate syntax 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace delegate_syntax_2
+{
+    class Program
+    {
+        public delegate bool TryGet<T1, T2>(T1 questionText, Action<T1> tellUser, out T2 age);
+
+        public static void Main()
+        {
+            Run(AskUser, Console.WriteLine);
+        }
+
+        public static void Run(TryGet<string, int> askUser, Action<string> tellUser)
+        {
+            int age;
+            if (askUser("What is your age?", tellUser, out age))
+                tellUser("Age: " + age);
+        }
+
+        public static bool AskUser(string questionText, Action<string> tellUser, out int age)
+        {
+            tellUser(questionText);
+            var answer = Console.ReadLine();
+            return int.TryParse(answer, out age);
+        }
+    }
+}
