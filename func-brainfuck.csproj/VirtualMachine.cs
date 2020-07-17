310a010c59753638acf6c584e91cb5b94be7335d
RufusBarma <neymanyu@gmail.com>
2020-07-17 16:16:11 +0500

    Solve the practice of "Brainfuck Virtual machine»

--- func-brainfuck.csproj/VirtualMachine.cs
+++ func-brainfuck.csproj/VirtualMachine.cs
@@ -5,22 +5,41 @@ namespace func.brainfuck
 {
 	public class VirtualMachine : IVirtualMachine
 	{
-		public VirtualMachine(string program, int memorySize)
+        private byte[] memory;
+
+        public byte[] Memory { get { return memory; } }
+
+        public int MemorySize { get { return memory.Length; } }
+
+        public int MemoryPointer { get; set; }
+
+        private Dictionary<char, Action<IVirtualMachine>> commands;
+
+        private List<char> instructions;
+
+        public string Instructions { get { return new string(instructions.ToArray()); } }
+
+        public int InstructionPointer { get; set; }
+
+        public VirtualMachine(string program, int memorySize)
 		{
+            memory = new byte [memorySize];
+            instructions = new List<char>(program.ToCharArray());
+            commands = new Dictionary<char, Action<IVirtualMachine>>();
+            MemoryPointer = 0;
+            InstructionPointer = 0;
 		}
 
 		public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
 		{
-			throw new NotImplementedException();
+            commands.Add(symbol, execute);
 		}
 
-		public string Instructions { get; }
-		public int InstructionPointer { get; set; }
-		public byte[] Memory { get; }
-		public int MemoryPointer { get; set; }
 		public void Run()
 		{
-			throw new NotImplementedException();
+			for(; InstructionPointer < Instructions.Length; InstructionPointer++)
+                if (commands.ContainsKey(Instructions[InstructionPointer]))
+                    commands[Instructions[InstructionPointer]](this);            
 		}
 	}
 }
