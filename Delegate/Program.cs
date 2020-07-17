ff6d21ccd1ba0b42fa6b29798b70c17a98dc2d20
RufusBarma <neymanyu@gmail.com>
2020-07-17 15:20:52 +0500

    Add solution in delegate

---
+++ Delegate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new Func<int, int[]>[3];
+            f[0] = (x) => new int[x];
+            int result = (f[0](0))[0];
+        }
+    }
+}
