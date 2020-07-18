72895a3b99997e9d94d8a572ad940fa924ceaeff
RufusBarma <neymanyu@gmail.com>
2020-07-18 14:13:38 +0500

    Solve the practice " Simple Brainfuck commands»

--- func-brainfuck.csproj/BrainfuckBasicCommands.cs
+++ func-brainfuck.csproj/BrainfuckBasicCommands.cs
@@ -8,10 +8,37 @@ namespace func.brainfuck
 	{
 		public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
 		{
-			vm.RegisterCommand('.', b => { });
-			vm.RegisterCommand('+', b => {});
-			vm.RegisterCommand('-', b => {});
-			//...
-		}
+			vm.RegisterCommand('.', b => { write((char)b.Memory[b.MemoryPointer]); });
+			vm.RegisterCommand('+', b => {
+                if (b.Memory[b.MemoryPointer] < 255)
+                    b.Memory[b.MemoryPointer]++;
+                else
+                    b.Memory[b.MemoryPointer] = 0;
+            });
+			vm.RegisterCommand('-', b => {
+                if (b.Memory[b.MemoryPointer] >0)
+                    b.Memory[b.MemoryPointer]--;
+                else
+                    b.Memory[b.MemoryPointer] = 255;
+            });
+			vm.RegisterCommand(',', b => { b.Memory[b.MemoryPointer] = (byte)read(); });
+            vm.RegisterCommand('>', b => {
+                if (b.MemoryPointer < b.Memory.Length - 1)
+                    b.MemoryPointer++;
+                else
+                    b.MemoryPointer = 0;
+            });
+            vm.RegisterCommand('<', b => {
+                if (b.MemoryPointer > 0)
+                    b.MemoryPointer--;
+                else
+                    b.MemoryPointer = b.Memory.Length - 1;
+            });
+            foreach (var c in "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890".ToCharArray())
+            {
+                var symbol = c;
+                vm.RegisterCommand(symbol, b => { b.Memory[b.MemoryPointer] = (byte)symbol; });
+            }
+        }
 	}
 }
