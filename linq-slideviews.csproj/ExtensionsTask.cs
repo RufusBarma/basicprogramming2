7558a9de1522bcc2cabe5c5d0634de1a43166276
RufusBarma <neymanyu@gmail.com>
2020-08-26 19:44:47 +0500

    Solved "Median and Bigrams"

--- linq-slideviews.csproj/ExtensionsTask.cs
+++ linq-slideviews.csproj/ExtensionsTask.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework.Constraints;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,17 +14,39 @@ namespace linq_slideviews
 		/// </summary>
 		/// <exception cref="InvalidOperationException">Если последовательность не содержит элементов</exception>
 		public static double Median(this IEnumerable<double> items)
-		{
-			throw new NotImplementedException();
+		{			
+			var sortedItems = items.OrderBy(item => item).ToList();
+			int count = sortedItems.Count();
+			if (count == 0)
+				throw new InvalidOperationException();
+			double median;
+			
+			if (count % 2 == 0)
+				median = (sortedItems.ElementAt(count / 2) + sortedItems.ElementAt((count - 1) / 2)) / 2;
+			else
+				median = sortedItems.ElementAt(count / 2);
+			return median;
 		}
 
 		/// <returns>
 		/// Возвращает последовательность, состоящую из пар соседних элементов.
 		/// Например, по последовательности {1,2,3} метод должен вернуть две пары: (1,2) и (2,3).
 		/// </returns>
+		/// <exception cref="InvalidOperationException">Если последовательность не содержит элементов</exception>
 		public static IEnumerable<Tuple<T, T>> Bigrams<T>(this IEnumerable<T> items)
 		{
-			throw new NotImplementedException();
+			var itemsEnumerator = items.GetEnumerator();
+			T prevItem;
+			if (itemsEnumerator.MoveNext())
+				prevItem = itemsEnumerator.Current;
+			else
+				throw new InvalidOperationException();
+			
+			while (itemsEnumerator.MoveNext())
+			{      
+				yield return Tuple.Create(prevItem, itemsEnumerator.Current);
+				prevItem = itemsEnumerator.Current;
+			}			
 		}
 	}
 }
