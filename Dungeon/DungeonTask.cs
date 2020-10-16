9404018def6fde692de28f61f20b4263534e7fa8
RufusBarma <neymanyu@gmail.com>
2020-10-16 23:45:00 +0500

    First solution of Dungeon Task. For local tests it's success

--- Dungeon/DungeonTask.cs
+++ Dungeon/DungeonTask.cs
@@ -12,7 +12,40 @@ namespace Dungeon
 	{
 		public static MoveDirection[] FindShortestPath(Map map)
 		{
-			return new MoveDirection[0];
+			var startToChests = BfsTask.FindPaths(map, map.InitialPosition, map.Chests);
+			if (startToChests == null)
+			{
+				var startToExit = BfsTask.FindPaths(map, map.InitialPosition, new Point[1] {map.Exit});
+				return PointsToMoveDirection(startToExit.First());
+			}
+
+			var exitToChests = BfsTask.FindPaths(map, map.Exit, map.Chests).Reverse();
+			var startToChestToExit = startToChests
+				.Join(exitToChests, sToCh => sToCh.Value, eToCh => eToCh.Value,
+				(sToCh, eToCh) =>
+				{
+					var connect = new SinglyLinkedList<Point>(eToCh.Value, sToCh);
+					return eToCh.Skip(1).Aggregate(connect, (current, point) => new SinglyLinkedList<Point>(point, current));
+				});
+			var shortest = startToChestToExit.OrderBy(list => list.Length).First();
+			return PointsToMoveDirection(shortest);
+		}
+
+		private static MoveDirection[] PointsToMoveDirection(SinglyLinkedList<Point> path)
+		{
+			return path
+				.Zip(path.Skip(1), ((leftPoint, rightPoint) =>
+				{
+					if (rightPoint.X > leftPoint.X)
+						return MoveDirection.Right;
+					else if (rightPoint.X < leftPoint.X)
+						return MoveDirection.Left;
+					else if (rightPoint.Y > leftPoint.Y)
+						return MoveDirection.Up;
+					else
+						return MoveDirection.Down;
+				}))
+				.ToArray();
 		}
 	}
 }
