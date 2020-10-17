f807fafbb89cbddfe6cfb726335d920a05728666
RufusBarma <neymanyu@gmail.com>
2020-10-17 23:13:29 +0500

    All tests are done, but the last isn't.

--- Dungeon/DungeonTask.cs
+++ Dungeon/DungeonTask.cs
@@ -12,38 +12,50 @@ namespace Dungeon
 	{
 		public static MoveDirection[] FindShortestPath(Map map)
 		{
-			var startToChests = BfsTask.FindPaths(map, map.InitialPosition, map.Chests);
-			if (startToChests == null)
+			var startToChests = BfsTask.FindPaths(map, map.InitialPosition, map.Chests).ToList();
+			if (!startToChests.Any())
 			{
-				var startToExit = BfsTask.FindPaths(map, map.InitialPosition, new Point[1] {map.Exit});
-				return PointsToMoveDirection(startToExit.First());
+				var startToExit = BfsTask.FindPaths(map, map.InitialPosition, new Point[1] {map.Exit}).ToList();
+				if (!startToExit.Any())
+					return new MoveDirection[0];
+				var pathToExit = startToExit.OrderBy(list => list.Length).First();
+				return pathToExit == null ? new MoveDirection[0] : PointsToMoveDirection(pathToExit);
 			}
 
-			var exitToChests = BfsTask.FindPaths(map, map.Exit, map.Chests).Reverse();
+			var exitToChests = BfsTask.FindPaths(map, map.Exit, map.Chests);
+			
 			var startToChestToExit = startToChests
 				.Join(exitToChests, sToCh => sToCh.Value, eToCh => eToCh.Value,
 				(sToCh, eToCh) =>
 				{
 					var connect = new SinglyLinkedList<Point>(eToCh.Value, sToCh);
 					return eToCh.Skip(1).Aggregate(connect, (current, point) => new SinglyLinkedList<Point>(point, current));
-				});
-			var shortest = startToChestToExit.OrderBy(list => list.Length).First();
+				})
+				.Where(sTcTe => sTcTe != null);
+			var toChestToExit = startToChestToExit.ToList();
+			if (!toChestToExit.Any())
+				return new MoveDirection[0];
+			var shortest = toChestToExit.OrderBy(list => list.Length).First();
+
 			return PointsToMoveDirection(shortest);
 		}
 
-		private static MoveDirection[] PointsToMoveDirection(SinglyLinkedList<Point> path)
+		private static MoveDirection[] PointsToMoveDirection(IEnumerable<Point> path)
 		{
-			return path
-				.Zip(path.Skip(1), ((leftPoint, rightPoint) =>
+			var pathDistinct = path.Distinct().Reverse().ToList();
+			return pathDistinct
+				.Zip(pathDistinct.Skip(1), ((leftPoint, rightPoint) =>
 				{
 					if (rightPoint.X > leftPoint.X)
 						return MoveDirection.Right;
 					else if (rightPoint.X < leftPoint.X)
 						return MoveDirection.Left;
 					else if (rightPoint.Y > leftPoint.Y)
+						return MoveDirection.Down;
+					else if (rightPoint.Y < leftPoint.Y)
 						return MoveDirection.Up;
 					else
-						return MoveDirection.Down;
+						throw new Exception("Two points are similar");
 				}))
 				.ToArray();
 		}
