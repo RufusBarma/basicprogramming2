19ae795af09feac0c465e41685e2b43d7bb9f248
RufusBarma <neymanyu@gmail.com>
2020-10-20 21:18:31 +0500

    Upgrade PointsToMoveDirection

--- Dungeon/DungeonTask.cs
+++ Dungeon/DungeonTask.cs
@@ -12,14 +12,14 @@ namespace Dungeon
 	{
 		public static MoveDirection[] FindShortestPath(Map map)
 		{
-			var startToChests = BfsTask.FindPaths(map, map.InitialPosition, map.Chests).ToList();
+			var startToChests = BfsTask.FindPaths(map, map.InitialPosition, map.Chests);
 			if (!startToChests.Any())
 			{
-				var startToExit = BfsTask.FindPaths(map, map.InitialPosition, new Point[1] {map.Exit}).ToList();
+				var startToExit = BfsTask.FindPaths(map, map.InitialPosition, new Point[1] {map.Exit});
 				if (!startToExit.Any())
 					return new MoveDirection[0];
 				var pathToExit = startToExit.OrderBy(list => list.Length).First();
-				return pathToExit == null ? new MoveDirection[0] : PointsToMoveDirection(pathToExit);
+				return pathToExit == null ? new MoveDirection[0] : PointsToMoveDirection(pathToExit).ToArray();
 			}
 
 			var exitToChests = BfsTask.FindPaths(map, map.Exit, map.Chests);
@@ -32,32 +32,29 @@ namespace Dungeon
 					return eToCh.Skip(1).Aggregate(connect, (current, point) => new SinglyLinkedList<Point>(point, current));
 				})
 				.Where(sTcTe => sTcTe != null);
-			var toChestToExit = startToChestToExit.ToList();
-			if (!toChestToExit.Any())
+			if (!startToChestToExit.Any())
 				return new MoveDirection[0];
-			var shortest = toChestToExit.OrderBy(list => list.Length).First();
-
-			return PointsToMoveDirection(shortest);
+			var shortest = startToChestToExit.OrderBy(list => list.Length).First();
+			return PointsToMoveDirection(shortest).ToArray();
 		}
 
-		private static MoveDirection[] PointsToMoveDirection(IEnumerable<Point> path)
+		private static IEnumerable<MoveDirection> PointsToMoveDirection(IEnumerable<Point> path)
 		{
-			var pathDistinct = path.Distinct().Reverse().ToList();
-			return pathDistinct
-				.Zip(pathDistinct.Skip(1), ((leftPoint, rightPoint) =>
-				{
-					if (rightPoint.X > leftPoint.X)
-						return MoveDirection.Right;
-					else if (rightPoint.X < leftPoint.X)
-						return MoveDirection.Left;
-					else if (rightPoint.Y > leftPoint.Y)
-						return MoveDirection.Down;
-					else if (rightPoint.Y < leftPoint.Y)
-						return MoveDirection.Up;
-					else
-						throw new Exception("Two points are similar");
-				}))
-				.ToArray();
+			var leftPoint = path.Reverse().Take(path.Count()).GetEnumerator();
+			var rightPoint = path.Reverse().Skip(1).GetEnumerator();
+			while (leftPoint.MoveNext() && rightPoint.MoveNext())
+			{
+				if (leftPoint.Current == rightPoint.Current)
+					continue;
+				if (rightPoint.Current.X > leftPoint.Current.X)
+					yield return MoveDirection.Right;
+				else if (rightPoint.Current.X < leftPoint.Current.X)
+					yield return MoveDirection.Left;
+				else if (rightPoint.Current.Y > leftPoint.Current.Y)
+					yield return MoveDirection.Down;
+				else if (rightPoint.Current.Y < leftPoint.Current.Y)
+					yield return MoveDirection.Up;
+			}
 		}
 	}
 }
