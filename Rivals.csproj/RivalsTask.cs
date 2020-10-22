06047eeb6244421a2011eb1f8217c87ac256a544
RufusBarma <neymanyu@gmail.com>
2020-10-22 22:10:57 +0500

    Rivals task is done

--- Rivals.csproj/RivalsTask.cs
+++ Rivals.csproj/RivalsTask.cs
@@ -1,18 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Rivals
 {
-	public class RivalsTask
-	{
-		public static IEnumerable<OwnedLocation> AssignOwners(Map map)
-		{
-			yield break;
-		}
-	}
+    public class RivalsTask
+    {
+        public static IEnumerable<OwnedLocation> AssignOwners(Map map)
+        {
+            var visitedPoints = new HashSet<Point>();
+            var stack = new Queue<OwnedLocation>();
+            for (var i = 0; i < map.Players.Length; i++)
+                stack.Enqueue(new OwnedLocation(i, map.Players[i], 0));
+
+            while (stack.Count > 0)
+            {
+                var location = stack.Dequeue();
+                if (!map.InBounds(location.Location) ||
+                    map.Maze[location.Location.X, location.Location.Y] == MapCell.Wall ||
+                    visitedPoints.Contains(location.Location))
+                    continue;
+                visitedPoints.Add(location.Location);
+                stack.Enqueue(new OwnedLocation(location.Owner, new Point(location.Location.X + 1, location.Location.Y),
+                    location.Distance + 1));
+                stack.Enqueue(new OwnedLocation(location.Owner, new Point(location.Location.X - 1, location.Location.Y),
+                    location.Distance + 1));
+                stack.Enqueue(new OwnedLocation(location.Owner, new Point(location.Location.X, location.Location.Y + 1),
+                    location.Distance + 1));
+                stack.Enqueue(new OwnedLocation(location.Owner, new Point(location.Location.X, location.Location.Y - 1),
+                    location.Distance + 1));
+                yield return location;
+            }
+        }
+    }
 }
