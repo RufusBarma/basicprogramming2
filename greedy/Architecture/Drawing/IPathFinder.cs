d1c7365e66dddd55d369bc82713e9b5e1940bb5e
RufusBarma <neymanyu@gmail.com>
2020-12-08 12:56:44 +0500

    Greedy task

---
+++ greedy/Architecture/Drawing/IPathFinder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Greedy.Architecture.Drawing
+{
+	public interface IPathFinder
+	{
+		List<Point> FindPathToCompleteGoal(State state);
+	}
+}
---
+++ greedy/Architecture/Drawing/IScenePainter.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace Greedy.Architecture.Drawing
+{
+	public interface IScenePainter
+	{
+		SizeF Size { get; }
+		int CellSize { get; }
+		void Paint(Graphics g, float zoomScale);
+	}
+}
